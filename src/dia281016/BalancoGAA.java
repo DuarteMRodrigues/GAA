4029100cfded0f37e6508ebc663e6950e42400cb
UserPL135Pc17 <UserPL135Pc17@SLFRPL135PC17.ensino.atec.local>
2016-10-28 10:14:11 +0100

    :D

--- src/dia281016/BalancoGAA.java
+++ src/dia281016/BalancoGAA.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 
 public class BalancoGAA {
 	
-	int receitas, despesas;
-	
-	public BalancoGAA() {
-		receitas();
-		despesas();
-		apresentarBalanco();		
+	public BalancoGAA() {	
+		int receita = receitas();
+		int despesa = despesas();
+		
+		apresentarBalanco(receita, despesa);		
 	}
 	
-	public void receitas(){
-		int quotas, recBaile, recBar;
+	public int receitas(){
+		int quotas, recBaile, recBar, receitas;
 		
 		System.out.print("Introduza o valor das receitas\n\nQuotas: ");
 		quotas = (new Scanner(System.in)).nextInt();
@@ -25,10 +24,12 @@ public class BalancoGAA {
 		
 		receitas = quotas + recBaile + recBar;
 		System.out.print("\nO valor total das receitas é: " + receitas + "€");
+		
+		return receitas;
 	}
 	
-	public void despesas(){
-		int agua, luz, telefone; 
+	public int despesas(){
+		int agua, luz, telefone, despesas; 
 		
 		System.out.print("\n\n\nIntroduza o valor das despesas\n\nAgua: ");
 		agua = (new Scanner(System.in)).nextInt();
@@ -39,12 +40,15 @@ public class BalancoGAA {
 		
 		despesas = agua + luz + telefone;
 		System.out.print("\nO valor total das despesas é: " + despesas + "€");
+		
+		return despesas;
 	}
 	
-	public void apresentarBalanco(){
+	public void apresentarBalanco(int receitas, int despesas){
 		int balanco;
 		
 		balanco = receitas - despesas;
-		System.out.print("\n\n\nO balanço é: " + balanco + "€");	
+		System.out.print("\n\n\nO balanço é: " + balanco + "€");
 	}
 }
+
