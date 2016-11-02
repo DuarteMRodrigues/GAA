29b4d4ee50bce63f27f4b090f366a77e9f822730
UserPL135Pc17 <UserPL135Pc17@SLFRPL135PC17.ensino.atec.local>
2016-11-02 10:43:11 +0000

    hey

--- src/dia021116/Calculadora.java
+++ src/dia021116/Calculadora.java
@@ -3,66 +3,64 @@ package dia021116;
 import java.util.Scanner;
 
 public class Calculadora {
-	int num2 = 1;
 	
 	public Calculadora() {
-		int num1 = 0, escolha = 0, resultado = 0;
+		int num1 = 0, num2 = 1, escolha = 0, resultado = 0;
+		
+		System.out.print("Introduza o primeiro numero: ");
+		num1 = (new Scanner(System.in)).nextInt();
+		System.out.print("Introduza o segundo numero: ");
+		num2 = (new Scanner(System.in)).nextInt();
 		
 		while (num2 != 0){
-			System.out.print("Introduza um numero: ");
-			num1 = (new Scanner(System.in)).nextInt();
-			
-			System.out.println("Escolha uma operação:");
-			System.out.print("1 - Somar\n2 - Subtrair\n3 - Multiplicar\n4 - Dividir\nEscolha: ");
-			escolha = (new Scanner(System.in)).nextInt();
+		
+				System.out.println("Escolha uma operação:");
+				System.out.print("1 - Somar\n2 - Subtrair\n3 - Multiplicar\n4 - Dividir\nEscolha: ");
+				escolha = (new Scanner(System.in)).nextInt();
 			
 			switch (escolha){
 			case 1:
-				resultado = somar(num1);
+				resultado = somar(num1, num2);
 				System.out.println("O resultado é: " + resultado);
 				break;
 			case 2:
-				resultado = subtrair(num1);
+				resultado = subtrair(num1, num2);
 				System.out.println("O resultado é: " + resultado);
 				break;
 			case 3:
-				resultado = multiplicar(num1);
+				resultado = multiplicar(num1, num2);
 				System.out.println("O resultado é: " + resultado);
 				break;
 			case 4:
-				resultado = dividir(num1);
+				resultado = dividir(num1, num2);
 				System.out.println("O resultado é: " + resultado);
 				break;
+			default:
+				System.out.println("Operação inválida");
+				break;
 			}
+			System.out.print("Introduza o primeiro numero: ");
+			num1 = (new Scanner(System.in)).nextInt();
+			System.out.print("Introduza o segundo numero: ");
+			num2 = (new Scanner(System.in)).nextInt();
 		}
+		System.out.print("End");
 	}
 
-	public int somar(int num1){
-		System.out.print("Introduza o segundo numero: ");
-		num2 = (new Scanner(System.in)).nextInt();
-		
+	public int somar(int num1, int num2){
 		return num1 + num2;
 	}
 	
-	public int subtrair(int num1){
-		System.out.print("Introduza o segundo numero: ");
-		num2 = (new Scanner(System.in)).nextInt();
-		
+	public int subtrair(int num1, int num2){
 		return num1 - num2;	
 	}
 	
-	public int multiplicar(int num1){
-		System.out.print("Introduza o segundo numero: ");
-		num2 = (new Scanner(System.in)).nextInt();
-		
+	public int multiplicar(int num1, int num2){
 		return num1 * num2;
 	}
 	
-	public int dividir(int num1){
-		System.out.print("Introduza o segundo numero: ");
-		num2 = (new Scanner(System.in)).nextInt();
-		
-		return num1 / num2;
+	public int dividir(int num1, int num2){
+			return num1 / num2;
 	}
 
 	
